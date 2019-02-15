dba847d416a005309601fc88b103c286af2afaf9
Kate Zebrose <katezebrose@gmail.com>
2019-02-14 22:41:41 -0500

    Changed filename code in  walk

--- walk_action_page.php
+++ walk_action_page.php
@@ -6,19 +6,20 @@
   function getLogHandle($name,$ver)
   {
     $applicationPath = dirname(__FILE__);
-    $dateString = date("wH_D_F_j_Y_ha_");
-    $currentLogName = $applicationPath."/".$dateString.$name.".html";
+    $currentLogName = $applicationPath."/".$name.".html";
     $logh = fopen($currentLogName, "a+") or die("Unable to open $currentLogName");
-    $txt=" === v$ver === $name ".date("Y/m/d H:i:s")."\n";
-    //fwrite($logh, $txt);
     return $logh;
   }
 
   function writeForm($data)
   {
-    $date = preg_replace(array('/ /','/,/'),array("_","_"),$data['clock1']);
-    $time = preg_replace(array('/:/','/,/'),array("_","_"),$data['clock2']);
-    $logh = getLogHandle($date.$time.'exercise','0.1');
+    $date = $data['clock1'];
+    $time = $data['clock2'];
+    echo "string $date $time";
+    $timeStamp = strtotime("$date $time");
+    echo $timeStamp;
+    $dateString = date("wH_D_F_j_Y_ha_",$timeStamp);
+    $logh = getLogHandle($dateString.'exercise','1.0');
     foreach($data as $key => $line)
     {
       if(strpos($key,"clock1") === 0)
