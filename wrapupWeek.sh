3ff2eb3381442a2502d262b8bb72a30a99de80ef
Kate Zebrose <katezebrose@gmail.com>
2019-03-10 10:02:12 -0400

    added prompt to wrap

--- wrapupWeek.sh
+++ wrapupWeek.sh
@@ -4,8 +4,9 @@ echo "Welcome, from wrapupWeek.sh, the script to move the weekending directory u
 echo "I need some help with naming the directory in the new location.";
 echo "The most recently added name is ...";
 ls -lrt ~/exercise/save|tail -n 1;
+default=$(ls -rt ~/exercise/save|tail -n 1);
 echo "Please enter the name you want for the current weekending directory.";
-read newName;
+read -e -p "Enter: " -i "$default" newName;
 
 #a new weekending directory needs to be created
 mv ~/exercise/weekending ~/exercise/save/$newName
