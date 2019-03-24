a1301f73b36a61b994eda18d9d6d579e678cc196
Kate Zebrose <katezebrose@gmail.com>
2019-03-23 20:19:59 -0400

    created new enhanced method for updating plots

--- plotting/pltAll.sh
+++ plotting/pltAll.sh
@@ -2,8 +2,16 @@
 
 # update plt files with todays date
 # this extends the x range to today
+
+#get string with todays date in required format
 today=$(php -f getToday.php)
 
+#use templates to create updated plt files
+sed "s/TODAY/$today/" 25ftwalk.template>25ftwalk.plt
+sed "s/TODAY/$today/" 2minWalk.template>2minWalk.plt
+sed "s/TODAY/$today/" tug.template>tug.plt
+
+#use plt files to create png files
 gnuplot "25ftwalk.plt"
 gnuplot "2minWalk.plt"
 gnuplot "tug.plt"
