a41ec1b02111684e589a9d59e72e8f0d00897c16
Kate Zebrose <katezebrose@gmail.com>
2019-05-14 20:07:51 -0400

    changed wrapweek to use moveFiles

--- wrapupWeek.sh
+++ wrapupWeek.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 weekDir=~/exercise/weekending
-echo "Welcome, from wrapupWeek.sh, the script to move the weekending directory under the save directory";
-echo "I need some help with naming the directory in the new location.";
-echo "The most recently added name is ...";
-ls -lrt ~/exercise/save|tail -n 1;
+echo "Welcome to wrapupWeek.sh, the script to move the weekending directory under the save directory";
 default=$(ls -rt ~/exercise/save|tail -n 1);
-echo "Please enter the name you want for the current weekending directory.";
+echo "I have calculated that $default is the Saturday date for the last week processed.";
+echo "If the date looks correct type enter to confirm.";
+echo "If this is wrong then correct the date";
 read -e -p "Enter: " -i "$default" newName;
 
 #a new weekending directory needs to be created
@@ -14,41 +13,5 @@ mv $weekDir ~/exercise/save/$newName;
 mkdir $weekDir;
 cp ~/exercise/makeThisWeek.sh $weekDir;
 echo "I will now copy files from the following week into the new weekending directory";
-# nextWeek.php returns the date $newName + ? days 
-sunStr=$(php -f nextWeek.php $newName 1);
-if [ -f ${sunstr} ];
-then
-  mv $sunStr $weekDir;
-fi
-monStr=$(php -f nextWeek.php $newName 2);
-if [ -f ${monstr} ];
-then
-  mv $monStr $weekDir;
-fi
-tueStr=$(php -f nextWeek.php $newName 3);
-if [ -f ${tuestr} ];
-then
-  mv $tueStr $weekDir;
-fi
-wedStr=$(php -f nextWeek.php $newName 4);
-if [ -f ${wedstr} ];
-then
-  mv $wedStr $weekDir;
-fi
-thuStr=$(php -f nextWeek.php $newName 5);
-if [ -f ${thustr} ];
-then
-  mv $thuStr $weekDir;
-fi
-friStr=$(php -f nextWeek.php $newName 6);
-if [ -f ${fristr} ];
-then
-  mv $friStr $weekDir;
-fi
-satStr=$(php -f nextWeek.php $newName 7);
-if [ -f ${satstr} ];
-then
-  mv $satStr $weekDir;
-fi
-
+./moveFiles.sh
 echo "You are ready for a new week.  Happy exercising.";
