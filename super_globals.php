909a0a33f03351c04ba4fe57b3bda92e35e94cd7
Kate Zebrose <katezebrose@gmail.com>
2019-05-21 14:03:40 -0400

    added names to super global demo page

--- super_globals.php
+++ super_globals.php
@@ -3,17 +3,23 @@
 <body>
 
 <?php 
-echo $_SERVER['PHP_SELF'];
+$self = $_SERVER['PHP_SELF'];
+echo "_SERVER['PHP_SELF'] = $self";
 echo "<br>";
-echo $_SERVER['SERVER_NAME'];
+$name = $_SERVER['SERVER_NAME'];
+echo "_SERVER['SERVER_NAME'] = $name";
 echo "<br>";
-echo $_SERVER['HTTP_HOST'];
+$host =  $_SERVER['HTTP_HOST'];
+echo "_SERVER['HTTP_HOST'] = $host";
 echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
+$referer = $_SERVER['HTTP_REFERER'];
+echo "_SERVER['HTTP_REFERER'] = $referer";
 echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
+$userAgent =  $_SERVER['HTTP_USER_AGENT'];
+echo "_SERVER['HTTP_USER_AGENT'] = $userAgent";
 echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
+$script= $_SERVER['SCRIPT_NAME'];
+echo "_SERVER['SCRIPT_NAME'] = $script";
 ?>
 
 </body>
