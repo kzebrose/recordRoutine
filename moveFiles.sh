256756a030e0dd68bcdece247fd70d81a04ae22e
Kate Zebrose <katezebrose@gmail.com>
2019-05-30 00:19:30 -0400

    changed test used in movefiles, add error checking to nextWeek.php, swaped logo in endofweek report

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,83 +1,142 @@
 #!/usr/bin/env bash
 
+#first message doesn't make sense if called from wrapweek.sh
+#adding any argument removes the first message
+CALL_FROM_WRAPWEEK=${1}
+if [ -z ${CALL_FROM_WRAPWEEK} ];
+then
+  echo "Welcome to moveFiles!  I will help you move files into the weekending directory.";
+fi
+
+#nullglob effects how shell behaves when a 'glob' such as kat* does not match any file
+#my experiments indicate this setting works for my shell zsh
+#it allows me to get the length of a null list without an error
+shopt -s  nullglob
 weekDir=~/exercise/weekending
-echo "Welcome to moveFiles!  I will help you move files into the weekending directory.";
 default=$(ls -rt ~/exercise/save|tail -n 1);
 echo "I have calculated that $default is the Saturday date for the last week processed.";
 echo "If the date looks correct type enter to confirm.";
 echo "If this is wrong then correct the date";
 read -e -p "Enter: " -i "$default" newName;
-
 echo $newName;
-sunStr=$(php -f nextWeek.php $newName 1);
-for f in *$sunStr*; do
-  ## Check if glob gets expanded to file.
-  [ -e "$f" ] && flag=0 || flag=1
-  break;
+
+### processes Sunday files ###
+sunStr=$(php -f nextWeek.php $newName 1 file);
+#check if nextWeek detected an error
+if [[ $sunStr == *"ERROR"* ]]; 
+  then 
+    echo $sunStr;
+    exit; 
+fi
+#expand glob to a list of files in array list
+list=(*$sunStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no sunday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv *$file $weekDir;
 done
-if [ $flag == 0 ]; then
-  mv *$sunStr* $weekDir;
-  echo "move Sunday files";
-else
-  echo "no Sunday file to move";
+
+### processes Monday files ###
+monStr=$(php -f nextWeek.php $newName 2 file);
+#check if nextWeek detected an error
+if [[ $monStr == *"ERROR"* ]]; 
+  then 
+    echo $monStr;
+    exit; 
 fi
-monStr=$(php -f nextWeek.php $newName 2);
-for f in *$monStr*; do
-  ## Check if glob gets expanded to file.
-  [ -e "$f" ] && flag=0 || flag=1
-  break;
+#expand glob to a list of files in array list
+list=(*$monStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no monday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
 done
-if [ $flag == 0 ]; then
-  mv *$monStr* $weekDir;
-  echo "move Monday files";
-else
-  echo "no Monday file to move";
+
+### processes Tuesday files ###
+tueStr=$(php -f nextWeek.php $newName 3 file);
+#check if nextWeek detected an error
+if [[ $tueStr == *"ERROR"* ]]; 
+  then 
+    echo $tueStr;
+    exit; 
 fi
-tueStr=$(php -f nextWeek.php $newName 3);
-for f in *$tueStr*; do
-  ## Check if glob gets expanded to file.
-  [ -e "$f" ] && flag=0 || flag=1
-  break;
+#expand glob to a list of files in array list
+list=(*$tueStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no tuesday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
 done
-##only mv files if files exist
-if [ $flag == 0 ]; then
-  mv *$tueStr* $weekDir;
-  echo "move Tuesday file";
-else
-  echo "no Tuesday file to move";
+
+### processes Wednesday files ###
+wedStr=$(php -f nextWeek.php $newName 4 file);
+#check if nextWeek detected an error
+if [[ $wedStr == *"ERROR"* ]]; 
+  then 
+    echo $wedStr;
+    exit; 
 fi
-wedStr=$(php -f nextWeek.php $newName 4);
-for f in *$wedStr*; do
-  ## Check if glob gets expanded to file.
-  [ -e "$f" ] && flag=0 || flag=1
-  break;
+#expand glob to a list of files in array list
+list=(*$wedStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no wednesday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
 done
-##only mv files if files exist
-if [ $flag == 0 ]; then
-  mv *$wedStr* $weekDir;
-  echo "move Wednesday files";
-else
-  echo "no Wednesday file to move";
+
+### processes Thursday files ###
+thuStr=$(php -f nextWeek.php $newName 5 file);
+#check if nextWeek detected an error
+if [[ $thuStr == *"ERROR"* ]]; 
+  then 
+    echo $thuStr;
+    exit; 
 fi
-thuStr=$(php -f nextWeek.php $newName 5);
-if [ $flag == 0 ]; then
-  mv *$thuStr* $weekDir;
-  echo "move Thursday files";
-else
-  echo "no Thursday file to move";
+#expand glob to a list of files in array list
+list=(*$thuStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no thursday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
+done
+
+### processes Friday files ###
+friStr=$(php -f nextWeek.php $newName 6 file);
+#check if nextWeek detected an error
+if [[ $friStr == *"ERROR"* ]]; 
+  then 
+    echo $friStr;
+    exit; 
 fi
-friStr=$(php -f nextWeek.php $newName 6);
-if [ $flag == 0 ]; then
-  mv *$friStr* $weekDir;
-  echo "move Friday files";
-else
-  echo "no Friday file to move";
+#expand glob to a list of files in array list
+list=(*$friStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no friday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
+done
+
+
+### processes Saturday files ###
+satStr=$(php -f nextWeek.php $newName 7 file);
+#check if nextWeek detected an error
+if [[ $satStr == *"ERROR"* ]]; 
+  then 
+    echo $satStr;
+    exit; 
 fi
-satStr=$(php -f nextWeek.php $newName 7);
-if [ $flag == 0 ]; then
-  mv *$satStr* $weekDir;
-  echo "move Saturday files";
-else
-  echo "no Saturday file to move";
-fi
+#expand glob to a list of files in array list
+list=(*$satStr*);
+num=${#list[@]};
+if [[ ${#list[@]} == 0 ]]; then echo "there are no saturday files";else echo "there are $num files";fi;
+for file in "${list[@]}"; do 
+  echo "move $file to $weekDir"; 
+  mv $file $weekDir;
+done
 
--- nextWeek.php
+++ nextWeek.php
@@ -1,18 +1,39 @@
 <?php
+function usage()
+{
+  echo "ERROR usage:nextWeek.php <weekendingYYYYMMMDD> <offset> <format>\n";
+  echo "      offset is the number of days added to weekending date\n";
+  echo "      format is either 'dir' or 'file'\n\n";
+  echo "examples:\n          'dir' would be 'weekending2019May18'\n";
+  echo "          'file' would be 'Sat_May_18_2019'\n";
+}
 
-//script called from wrapupWeek.sh for date calculation
-if($argc > 2)
+//script called from move.sh and wrapupWeek.sh for date calculation
+if($argc > 3)
 {
   $lastName = $argv[1];
   $offset = $argv[2];
+  $format = $argv[3];
 }
 else
 {
-  echo "usage:nextWeek.php <weekendingYYYYMMMDD> <offset>\n";
-  echo "offset is the number of days added to weekending date\n";
+  usage();
   exit;
 }
 
+//check name starts with weekending
+$prefix = substr($lastName,0,10);
+if($prefix != "weekending")
+{
+  echo "ERROR:name MUST start with weekending NOT $prefix - nextWeek.php\n";
+  exit;
+}
+$len = strlen($lastName);
+if($len != 19)
+{
+  echo "ERROR:name MUST have 19 characters - NOT $len nextWeek.php\n";
+  exit;
+}
 //parse directory name from last week into a unix timestamp
 $lastDate = substr($lastName,10);
 //echo $lastDate."\n";
@@ -20,6 +41,20 @@ $lastDateObj = date_create_from_format('YFj',$lastDate);
 //echo $lastDateObj->format('m-d-Y')."\n";
 $dateFormat = "P".$offset."D";
 $nextDateObj = $lastDateObj->add(new DateInterval($dateFormat));
-echo $nextDateObj->format('D_F_j_Y');
+if ($format == 'file')
+{
+  echo $nextDateObj->format('D_F_j_Y');
+}
+elseif ($format == 'dir')
+{
+  $newDate = $nextDateObj->format('YFj');
+  echo "weekending$newDate";
+}
+else
+{
+  echo "\n\nERROR: $format is not a supported format\n\n";
+  usage();
+}
+
 
 ?>
