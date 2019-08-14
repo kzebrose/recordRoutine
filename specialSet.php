eee086391c014fe2c199dd9d267772d839070adb
Kate Zebrose <katezebrose@gmail.com>
2019-08-14 17:47:33 -0400

    fix bug in calculating times for special set

--- specialSet.php
+++ specialSet.php
@@ -67,7 +67,7 @@ include ("./mobile_header.php");
       </div><!-- end col -->
       <div class="col-xs-12 col-md-6">
          <div class="row"><div class="col-xs-12 col-md-6"><!-- inner row 2 -->
-           <button type="button" class="btn btn-success" onclick="setEndTimeExercise('set1startTime','setSendTime')">End</button>
+           <button type="button" class="btn btn-success" onclick="setEndTimeExercise('set1startTime','set1endTime')">End</button>
          </div><!-- end first col of inner row 2 --><div class="col-xs-12 col-md-6"><!-- second col of inner row 2 -->
            <input  id=set1endTime    type="text" name="set1endTime" value="09:00">
          </div><!-- end second col of inner row 2 --></div><!-- end inner row 2 -->
